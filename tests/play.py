b45532438e4d2649c8a47ca330638351edb47ff9
Calebe94 <calebe.sep@hotmail.com>
2018-11-09 11:58:58 -0200

    <refactor> removed playsound and pygle dependencys, and added mutagen dependency

--- tests/play.py
+++ tests/play.py
@@ -1,11 +1,19 @@
 import vlc
 import os
+import time
+from mutagen.mp3 import MP3
+
+
 print(os.path.dirname(os.path.abspath(__file__)))
 
 AUDIO_PATH = os.path.dirname(os.path.abspath(__file__)).replace("tests", "res/audio")
 
 print(AUDIO_PATH)
 
+audio = MP3(AUDIO_PATH+"/donald_duck_laugh.mp3")
+print(audio.info.length)
+
 p = vlc.MediaPlayer(AUDIO_PATH+"/donald_duck_laugh.mp3")
-# p = vlc.MediaPlayer("file:///"+AUDIO_PATH+"/donald_duck_laugh.mp3")
 p.play()
+
+time.sleep(int(audio.info.length))
--- tests/playsound.py
+++
@@ -1,9 +0,0 @@
-# import playsound
-from playsound import playsound
-import os
-# print(os.path.dirname(os.path.abspath(__file__)))
-
-AUDIO_PATH = os.path.dirname(os.path.abspath(__file__)).replace("tests", "res/audio")
-
-print(AUDIO_PATH+'/donald_duck_laugh.mp3')
-# playsound.playsound(AUDIO_PATH+'/donald_duck_laugh.mp3', True)
