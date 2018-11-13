3b89a1a3ee7865d51ad137a222138b7548b715ed
Calebe94 <calebe.sep@hotmail.com>
2018-11-13 13:05:43 -0200

    time controll added

--- src/main.py
+++ src/main.py
@@ -10,9 +10,10 @@ from presence import Presence
 from motor import Motor
 from constants import DONALD_TRACK
 
-# AUDIO_PATH = os.path.dirname(os.path.abspath(__file__)).replace("src", "res/audio")
+AUDIO_PATH = os.path.dirname(os.path.abspath(__file__)).replace("src", "res/audio")
 
-# audio = MP3(AUDIO_PATH+"/donald_duck_laugh.mp3")
+audio = MP3(AUDIO_PATH+"/"+DONALD_TRACK)
+print(audio.info.length)
 print("> Ready to use...")
 def main():
     motor = Motor()
@@ -23,10 +24,26 @@ def main():
         if presence.has_presence() == True:
             motor.on()
             print("> Motor ON!")
-            # donaldduck.play("donald_duck_laugh.mp3")
-            # donaldduck.play_donald_laugh()
-            # donaldduck.play_random()
-            donaldduck.play(DONALD_TRACK)
+            # donaldduck.play(DONALD_TRACK)
+
+            p = vlc.MediaPlayer(AUDIO_PATH+"/"+DONALD_TRACK)
+            p.play()
+            start = time.time()
+
+            while audio.info.length > (time.time() - start):
+                if ((time.time()-start) > 0.0) and (time.time()-start) < 1.0:
+                    motor.on()
+                elif ((time.time()-start) > 1.1) and ((time.time()-start) < 20.0):
+                    motor.off()
+                elif ((time.time()-start) > 20.1) and ((time.time()-start) < 22.0):
+                    motor.on()
+                elif ((time.time()-start) > 21.1):
+                    motor.off()
+                    
+                
+                    
+            # time.sleep(int(audio.info.length))
+            # Measure Elapsed Time
             print("> Playing Track!")
             motor.off()
             print("> Motor OFF!")
